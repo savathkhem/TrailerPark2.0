31e4f5a39fee6274fa7e6b2ad858e8616b6ad027
Nathan Geckle <nathan_geckle@yahoo.com>
2018-06-23 13:32:17 -0400

    Navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Logo from "../Logo";
+import NavBar from "../NavBar";
 
 const Nav = () => (
   <nav class="nav-extended grey darken-3">
     <div class="nav-wrapper">
     <Logo />
+    <NavBar />
     </div>
   </nav>
 );
