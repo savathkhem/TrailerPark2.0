a708cdea6ed896d5948fcb8f6666703598471e1b
Unknown <savath.khem@live.com>
2018-06-25 12:03:21 -0400

    Auto stash before merge of "master" and "origin/master"

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,14 +4,12 @@ import "./Card.css";
 const Card = (props) => (
         <div className="card">
             <div className="card-image waves-effect waves-block waves-light">
-                {/* <img className="modal-trigger movie-poster" data-title = "${title}" src="${posterPath}" href="#modal1"> */}
                 <img src={props.src} onClick={props.onClick} alt={props.title}/>  
-                {/* <a href="http://www.google.com" style="position:absolute;right:5px;bottom:5px" className="star-btn btn-floating yellow darken-1"><i className="material-icons">star_border</i></a> */}
             </div>
             <div className="card-content">
                 <span className="card-title activator grey-text text-darken-4">
                     <div id="modal-btn-container">
-                        <a id="modal-btn" className="waves-effect waves-light btn modal-trigger" href="#modal1">{props.title}</a>
+                        <a id="modal-btn" className="waves-effect waves-light btn modal-trigger" onClick={props.onClick}>{props.title}</a>
                         <i id="more-vert-btn" className="material-icons right">more_vert</i>
                     </div>
                 </span>
