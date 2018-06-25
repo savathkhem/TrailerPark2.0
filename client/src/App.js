bc5c7fd0bbccbe2d3be52660aab3922ea572079c
Unknown <savath.khem@live.com>
2018-06-25 12:13:51 -0400

    material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,8 @@
 import React, { Component } from "react";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "./components/Nav";
-import Card from "./components/Card";
-import Wrapper from "./components/Wrapper";
-import CardWrapper from "./components/CardWrapper";
-import API from "./utils/API";
-import Modal from "./components/Modal";
-import iFrame from "./components/iFrame";
 import AppBar from "./components/AppBar";
+import Home from "./pages/Home";
 import "./App.css"
 
 class App extends Component {
