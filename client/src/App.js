6360b1d02c9ef3a9c2227c41cf3c9bf871bba335
Unknown <savath.khem@live.com>
2018-06-25 12:07:34 -0400

    Auto stash before merge of "master" and "origin/master"

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
-// import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
-import Home from "./pages/Home";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import CardWrapper from "./components/CardWrapper";
+import API from "./utils/API";
+import Modal from "./components/Modal";
+import iFrame from "./components/iFrame";
+import AppBar from "./components/AppBar";
+import "./App.css"
 
 class App extends Component {
 
@@ -10,8 +17,8 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Nav />
-          <hr />
+        <CssBaseline />
+          <AppBar />
           <Route exact path="/" component={Home}/>
         </div>
       </Router>
