94f7100ade227aa26fc0bcb9c204692459f5867d
Nathan Geckle <nathan_geckle@yahoo.com>
2018-06-26 18:07:35 -0400

    Pulled

--- client/src/App.js
+++ client/src/App.js
@@ -9,33 +9,6 @@ import "./App.css"
 class App extends Component {
 
   render() {
-<<<<<<< HEAD
-    let toggleModal;
-    if (this.state.modal === true){
-      toggleModal = "show";
-    }
-    else {
-      toggleModal = "modal";
-    }
-    return (
-      <Router>
-        <div>
-          <Modal modal= {toggleModal} onClick= {this.closeModal}>
-            {this.state.youTubes.map((video) => (
-              <iFrame src= { "https://www.youtube.com/embed/" + video.id.videoId } />
-            ))}
-          </Modal>
-          <Nav />
-          <hr />
-          <Wrapper>
-            <CardWrapper>
-              {/* {this.state.movies.map((movie) => (
-                <Card key={movie.id} src={tmdbImgUrl + movie.poster_path} alt={movie.title} title={movie.title} onClick={()=>this.clickPoster(movie.title)}/>
-              ))} */}
-              <Home />
-            </CardWrapper>
-          </Wrapper>
-=======
     const path = <Route exact path="/" component={Home}/>;
     return (
       <Router>
@@ -44,7 +17,6 @@ class App extends Component {
           <AppBar />
           {/* <DrawerLeft /> */}
           <Route exact path="/" component={Home}/>
->>>>>>> 522099514ca8ccf18af25708eb336840ad2d901d
         </div>
       </Router>
     )
