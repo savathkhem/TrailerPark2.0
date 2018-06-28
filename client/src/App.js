d1f09b2069be7676e9625693447718fec84fef69
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2018-06-28 15:11:41 -0400

    firebase started

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,145 @@ import AppBar from "./components/AppBar";
 // import DrawerLeft from "./components/DrawerLeft";
 import Home from "./pages/Home";
 import "./App.css"
+import firebase from "./firebaseConfig";
+import withFirebaseAuth from "react-auth-firebase";
 
 class App extends Component {
+  state = {
+    email: `test@test.com`,
+    password: `password`
+  };
 
   render() {
-    
+    const {
+      signInWithEmail,
+      signUpWithEmail,
+      signInWithGoogle,
+      signInWithFacebook,
+      signInWithGithub,
+      signInWithTwitter,
+      googleAccessToken,
+      facebookAccessToken,
+      githubAccessToken,
+      twitterAccessToken,
+      twitterSecret,
+      user,
+      error,
+      signOut
+    } = this.props;
+    const { email, password } = this.state;
+    if (user) {
+      return <Home user={user} error={error} signOut={signOut} />;
+    }
+
     return (
       <Router>
         <div>
-        <CssBaseline />
-          <AppBar />
+          <div>
+            <form onSubmit={e => e.preventDefault()}>
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={e => this.setState({ email: e.target.value })}
+              />{" "}
+              <br />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+              <br />
+              {!user && (
+                <button
+                  type="submit"
+                  onClick={() => signInWithEmail(email, password)}
+                >
+                  SignIn
+            </button>
+              )}
+            </form>
+            <form onSubmit={e => e.preventDefault()}>
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={e =>
+                  this.setState({
+                    email: e.target.value
+                  })
+                }
+                value={email}
+              />{" "}
+              <br />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={e => this.setState({ password: e.target.value })}
+                value={password}
+              />{" "}
+              <br />
+              <button
+                type="submit"
+                onClick={() => signUpWithEmail(email, password)}
+              >
+                SignUp
+          </button>
+            </form>
+            <br />
+            <button onClick={signInWithGoogle}>Signin with Google</button> <br />
+            <button onClick={signInWithFacebook}>Signin with Facebook</button>{" "}
+            <br />
+            <button onClick={signInWithGithub}>Signin with Github</button> <br />
+            <button onClick={signInWithTwitter}>Signin with Twitter</button> <br />
+          </div>
           {/* <DrawerLeft /> */}
-          <Route exact path="/" component={Home}/>
+          <CssBaseline/>
+          <AppBar />
+          <Route exact path="/" component={Home} />
         </div>
       </Router>
     )
   }
 }
 
-export default App;
+const authConfig = {
+  email: {
+    verifyOnSignup: false,
+    saveUserInDatabase: true
+  },
+  google: {
+    // scopes: ["admin.datatransfer", "contacts.readonly"], // optional
+    // customParams: {
+    //   login_hint: "user@example.com"
+    // },
+    // redirect: true, // default is popup: true, redirect: true,
+    returnAccessToken: true,
+    // scopes: [], // array
+    saveUserInDatabase: true
+  },
+  facebook: {
+    // scopes: ["admin.datatransfer", "contacts.readonly"], // optional
+    // customParams: {
+    //   login_hint: "user@example.com"
+    // },
+    redirect: true, // default is popup: true, redirect: true,
+    returnAccessToken: true,
+    saveUserInDatabase: true
+  },
+  github: {
+    // redirect: true,
+    returnAccessToken: true,
+    saveUserInDatabase: true
+  },
+
+  twitter: {
+    // redirect: true,
+    returnAccessToken: true,
+    returnSecret: true,
+    saveUserInDatabase: true
+  }
+};
+
+
+
+// export default App;
+export default withFirebaseAuth(App, firebase, authConfig);
