c9a410a6bc750e0ee84e04486f37b951259d8d98
Nathan Geckle <nathan_geckle@yahoo.com>
2018-07-10 21:20:32 -0400

    Deleted a couple imports

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import ProfileWrapper from "../components/ProfileWrapper";
 import Grid from '@material-ui/core/Grid';
 import "./Home.css";
 
