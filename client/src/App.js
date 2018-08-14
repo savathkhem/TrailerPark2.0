b167878f2a0aecf0d3d9087cc7b9168eaee11205
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2018-08-13 22:30:51 -0400

    landing page no more. login in nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { Redirect, BrowserRouter as Router, Route} from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { Redirect, BrowserRouter as Router, Route } from "react-router-dom";
 import { AppBar } from "./components/Layout";
 import Login from "./components/Login";
 import Landing from "./components/Landing";
@@ -10,14 +10,19 @@ import "./App.css";
 import firebase from "./firebaseConfig";
 import Search from "./pages/Search";
 
-const tmdbImgUrl = 'https://image.tmdb.org/t/p/w185';
+const tmdbImgUrl = "https://image.tmdb.org/t/p/w185";
 
 class App extends Component {
   state = {
-    loading: true, 
-    authenticated: false, 
+    loading: true,
+    authenticated: false,
     searchRedirect: false,
-    searchArr: []
+    searchArr: [],
+    currentUser: {
+      displayName: "Not Logged In",
+      photoURL: "./images/blank_user.png",
+      email: "none"
+    }
   };
 
   componentWillMount() {
@@ -28,95 +33,122 @@ class App extends Component {
           currentUser: user,
           loading: false
         });
-      let userObj = {
-        name: this.state.currentUser.displayName,
-        email: this.state.currentUser.email,
-        user_id: this.state.currentUser.uid,
-      }
-      API.saveUser(userObj)
-      } 
-      else {
+        let userObj = {
+          name: this.state.currentUser.displayName,
+          email: this.state.currentUser.email,
+          user_id: this.state.currentUser.uid
+        };
+        API.saveUser(userObj);
+      } else {
         this.setState({
           authenticated: false,
-          currentUser: null,
+          currentUser: {
+              displayName: "Not Logged In",
+              photoURL: "./images/blank_user.png",
+              email: "none"
+            },
           loading: false
         });
       }
-    })
+    });
+    this.forceUpdate();
   }
 
   checkPosterPaths(arr) {
-    let newArr = arr
-    newArr.map( (movie) => {
-      if (movie.poster_path === null){
-       return movie.poster_path = "./images/placeholder.jpg";
+    let newArr = arr;
+    newArr.map(movie => {
+      if (movie.poster_path === null) {
+        return (movie.poster_path = "./images/placeholder.jpg");
+      } else {
+        return (movie.poster_path = tmdbImgUrl + movie.poster_path);
       }
-      else{
-       return movie.poster_path = tmdbImgUrl + movie.poster_path;
-      }
-    })
+    });
     arr = newArr;
     return arr;
-  };
+  }
 
   componentDidUpdate() {
-    if (this.state.searchRedirect===true) {
-      this.setState({searchRedirect: false});
+    if (this.state.searchRedirect === true) {
+      this.setState({ searchRedirect: false });
     }
-  };
+  }
 
   handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
       [name]: value
     });
-  }
+  };
 
-  handleFormSubmit = (event) => {
+  handleFormSubmit = event => {
     event.preventDefault();
     API.getSearch(this.state.search)
-      .then((res) => {
+      .then(res => {
         console.log(res);
-        this.checkPosterPaths(res.data)
+        this.checkPosterPaths(res.data);
         return res;
       })
       .then(res => this.setState({ searchArr: res.data }))
-      .then(this.setState({searchRedirect: true}))
+      .then(this.setState({ searchRedirect: true }))
       .catch(err => console.log(err));
-  }
-    
+  };
+
   render() {
-    if (this.state.currentUser) {
-      return (
-        <Router>
-          <div>
-            <CssBaseline />
-            <AppBar 
-              src={this.state.currentUser.photoURL}
-              alt={this.state.currentUser.displayName} 
-              name={this.state.currentUser.displayName}
-              onChange={this.handleInputChange}
-              handleSubmit={this.handleFormSubmit}
-            />
-            {this.state.searchRedirect && <Redirect push to="/search"/>}
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/" render={()=><Home user={this.state.currentUser} />}/>
-            <Route exact path="/in-theaters" render={()=><InTheaters user={this.state.currentUser} />}/>
-            <Route exact path="/top-tv" render={()=><TopTV user={this.state.currentUser}/>}/>
-            <Route exact path="/upcoming" render={()=><Upcoming user={this.state.currentUser}/>}/>
-            <Route exact path="/top-movies" render={()=><TopMovie user={this.state.currentUser}/>}/>
-            <Route exact path="/search" render={()=><Search movies={this.state.searchArr} user={this.state.currentUser}/>}/>
-          </div>
-        </Router>
-      )
-    }
-    else {
-      return (
-        <Router>
-          <Landing />
-        </Router>
-      )
-    }
+    return (
+      <Router>
+        <div>
+          <CssBaseline />
+          <AppBar
+            src={this.state.currentUser.photoURL}
+            alt={this.state.currentUser.displayName}
+            name={this.state.currentUser.displayName}
+            onChange={this.handleInputChange}
+            handleSubmit={this.handleFormSubmit}
+          />
+          {this.state.searchRedirect && <Redirect push to="/search" />}
+          <Route
+            exact
+            path="/login"
+            render={() => <InTheaters user={this.state.currentUser} />}
+          />
+          <Route
+            exact
+            path="/"
+            render={() => <Home user={this.state.currentUser} />}
+          />
+          <Route
+            exact
+            path="/in-theaters"
+            render={() => <InTheaters user={this.state.currentUser} />}
+          />
+          <Route
+            exact
+            path="/top-tv"
+            render={() => <TopTV user={this.state.currentUser} />}
+          />
+          <Route
+            exact
+            path="/upcoming"
+            render={() => <Upcoming user={this.state.currentUser} />}
+          />
+          <Route
+            exact
+            path="/top-movies"
+            render={() => <TopMovie user={this.state.currentUser} />}
+          />
+          <Route
+            exact
+            path="/search"
+            render={() => (
+              <Search
+                movies={this.state.searchArr}
+                user={this.state.currentUser}
+              />
+            )}
+          />
+        </div>
+      </Router>
+    );
   }
 }
 
--- client/src/components/Layout/AppBar/AppBar.js
+++ client/src/components/Layout/AppBar/AppBar.js
@@ -1,120 +1,136 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, MenuItem, Typography, Menu, IconButton, Avatar } from '@material-ui/core/';
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  MenuItem,
+  Typography,
+  Menu,
+  IconButton,
+  Avatar
+} from "@material-ui/core/";
 import Logo from "../../Logo";
 import firebase from "../../../firebaseConfig";
 import DrawerLeft from "./../../DrawerLeft";
+import ModalNew from "../../ModalNew";
+import Login from "../../Login";
+
 
 const drawerWidth = 240;
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
-    flex: {
-    flex: 1,
+  flex: {
+    flex: 1
   },
   appFrame: {
     height: 430,
     zIndex: 1,
-    overflow: 'hidden',
-    position: 'relative',
-    display: 'flex',
-    width: '100%',
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+    width: "100%"
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    position: 'fixed',
-    transition: theme.transitions.create(['margin', 'width'], {
+    position: "fixed",
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+      duration: theme.transitions.duration.leavingScreen
+    })
   },
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
-  'appBarShift-left': {
-    marginLeft: drawerWidth,
+  "appBarShift-left": {
+    marginLeft: drawerWidth
   },
-  'appBarShift-right': {
-    marginRight: drawerWidth,
+  "appBarShift-right": {
+    marginRight: drawerWidth
   },
   avatar: {
-    margin: 10,
+    margin: 10
   },
   bigAvatar: {
     width: 60,
-    height: 60,
+    height: 60
   },
   avatarButton: {
-    height: 'inherit',
+    height: "inherit",
     marginLeft: 12,
     marginRight: 20,
-    float: 'right',
+    float: "right"
   },
   menuButton: {
     marginLeft: 12,
     marginRight: 20,
-    float: 'right',
+    float: "right"
   },
   hide: {
-    display: 'none',
+    display: "none"
   },
   drawerPaper: {
     // position: 'relative',
-    width: drawerWidth,
+    width: drawerWidth
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
   },
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+      duration: theme.transitions.duration.leavingScreen
+    })
   },
-  'content-left': {
-    marginLeft: -drawerWidth,
+  "content-left": {
+    marginLeft: -drawerWidth
   },
-  'content-right': {
-    marginRight: -drawerWidth,
+  "content-right": {
+    marginRight: -drawerWidth
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
-  'contentShift-left': {
-    marginLeft: 0,
+  "contentShift-left": {
+    marginLeft: 0
   },
-  'contentShift-right': {
-    marginRight: 0,
-  },
+  "contentShift-right": {
+    marginRight: 0
+  }
 });
 
 class NavBar extends React.Component {
   state = {
     open: false,
-    anchor: 'left',
+    anchor: "left",
     auth: true,
     anchorEl: null,
+    modalOpen: false
   };
-  
+
+  handleModalClick = () => {
+    this.setState(state => ({ modalOpen: !state.modalOpen }));
+  };
+
   handleChange = (event, checked) => {
     this.setState({ auth: checked });
   };
@@ -129,19 +145,22 @@ class NavBar extends React.Component {
 
   handleChangeAnchor = event => {
     this.setState({
-      anchor: event.target.value,
+      anchor: event.target.value
     });
   };
 
-  logOut = ()=> firebase.auth().signOut()
-  .then((success)=> {
-    // Sign-out successful.
-    console.log("User signed out")
-  })
-  .catch((error)=> {
-    // An error happened
-    console.log(error)
-  });
+  logOut = () =>
+    firebase
+      .auth()
+      .signOut()
+      .then(success => {
+        // Sign-out successful.
+        console.log("User signed out");
+      })
+      .catch(error => {
+        // An error happened
+        console.log(error);
+      });
 
   render() {
     const { classes } = this.props;
@@ -149,17 +168,36 @@ class NavBar extends React.Component {
     const { auth, anchorEl } = this.state;
     const openlogin = Boolean(anchorEl);
 
+    //Saves user for conditional rendering of login/logout functions
+    const user = this.props.name;
+    let loginToggle;
+
+    if (user === "Not Logged In") {
+      loginToggle = (
+          <MenuItem
+            onClick={this.handleModalClick}
+            onClose={this.handleModalClick}
+          >
+            Log In
+          </MenuItem>
+      );
+    } else {
+      loginToggle = <MenuItem onClick={this.logOut}>Log Out</MenuItem>;
+    }
+
     return (
       <div>
+        <ModalNew open={this.state.modalOpen} onClose={this.handleModalClick}>
+          <Login/>
+        </ModalNew>
         <AppBar
           className={classNames(classes.appBar, {
             [classes.appBarShift]: open,
-            [classes[`appBarShift-${anchor}`]]: open,
+            [classes[`appBarShift-${anchor}`]]: open
           })}
-          style={{backgroundColor: "#424242"}}
+          style={{ backgroundColor: "#424242" }}
         >
           <Toolbar disableGutters={!open}>
-
             {/* <IconButton
               color="inherit"
               aria-label="open drawer"
@@ -167,52 +205,69 @@ class NavBar extends React.Component {
               className={classNames(classes.menuButton, open && classes.hide)}>
               <MenuIcon />
             </IconButton> */}
-            <DrawerLeft onChange={this.props.onChange} handleSubmit={this.props.handleSubmit}/>
+            <DrawerLeft
+              onChange={this.props.onChange}
+              handleSubmit={this.props.handleSubmit}
+            />
 
-
-            <Typography variant="title" color="inherit" className={classes.flex}>
+            <Typography
+              variant="title"
+              color="inherit"
+              className={classes.flex}
+            >
               <Logo />
             </Typography>
             {auth && (
               <div>
                 <IconButton
                   className={classes.avatarButton}
-                  aria-owns={openlogin ? 'menu-appbar' : null}
+                  aria-owns={openlogin ? "menu-appbar" : null}
                   aria-haspopup="true"
                   onClick={this.handleMenu}
                   color="inherit"
                 >
-                <Avatar alt={this.props.name} className={(classes.avatar, classes.bigAvatar)}><img className={classes.bigAvatar} src={this.props.src} alt={this.props.alt}/></Avatar>
+                  <Avatar
+                    alt={this.props.name}
+                    className={(classes.avatar, classes.bigAvatar)}
+                  >
+                    <img className={classes.bigAvatar} src={this.props.src} />
+                  </Avatar>
                 </IconButton>
                 <Menu
                   id="menu-appbar"
                   anchorEl={anchorEl}
                   anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
+                    vertical: "top",
+                    horizontal: "right"
                   }}
                   transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
+                    vertical: "top",
+                    horizontal: "right"
                   }}
                   open={openlogin}
                   onClose={this.handleClose}
                 >
-                  <MenuItem onClick={this.handleClose}><Link to="/">{this.props.name}'s Profile</Link></MenuItem>
+                  <MenuItem onClick={this.handleClose}>
+                    <Link to="/">
+                      {this.props.name}
+                      's Profile
+                    </Link>
+                  </MenuItem>
                   <MenuItem onClick={this.handleClose}>Settings</MenuItem>
-                  <MenuItem onClick={this.logOut}>Logout</MenuItem>
+                  {loginToggle}
                 </Menu>
-            </div>)}
+              </div>
+            )}
           </Toolbar>
         </AppBar>
       </div>
     );
   }
-};
+}
 
 NavBar.propTypes = {
   classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
 };
 
 export default withStyles(styles, { withTheme: true })(NavBar);
